89f80a2a5b6300d79a6e0d12c315bd4ee35d9aad
GoldenAlpharex <jerego1234@hotmail.com>
2020-09-28 13:57:11 -0400

    Ya yeet, skeddadle

--- exercice.py
+++ exercice.py
@@ -14,10 +14,30 @@ def join_integers(numbers):
 	return int("".join([str(elem) for elem in numbers]))  # Big brain, à la place de créer une variable string, fait juste mettre "".whateverLaFonctionQueTuVeuxFaire
 
 def generate_prime_numbers(limit):
-	return [0]
+	# premiers = liste vide
+	premiers = []
+	# nombres = liste des entiers de 2 à limite
+	nombres = [i for i in range(2, limit+1)]
+	# TANT QUE nombres est non vide FAIRE
+	while nombres != []:
+		# Ajouter à premiers le premier entier de nombres
+		premiers.append(nombres[0])
+		# nombres = liste des entiers de nombres non multiples du premier
+		nombres = [elem for elem in nombres if elem % nombres[0] != 0]
+	return premiers
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [""]
+	# En plusieurs lignes, bonne façon de commencer avant de raccourcir ça en une seule ligne
+	# result = []
+	# for i in range(1, num_combinations+1):
+	# 	if excluded_multiples == None:
+	# 		result += [elem+str(i) for elem in strings]
+	# 	elif i % excluded_multiples != 0:
+	# 		result += [elem+str(i) for elem in strings]
+	#
+	# En deux lignes
+	# result = [elem+str(i) for elem in strings for i in range(1, num_combinations+1) if excluded_multiples == None or i % excluded_multiples != 0]
+	return [elem+str(i) for i in range(1, num_combinations+1) for elem in strings if excluded_multiples == None or i % excluded_multiples != 0]
 
 if __name__ == "__main__":
 	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
